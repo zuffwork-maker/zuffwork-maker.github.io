397c290d7890a382cbe1d2f0264077a4961db811
zuffwork-maker <zuffwork@gmail.com>
2026-09-17 08:05:15 +0700

    Add files via upload

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"a8bfdfc394deaed5c57bd45a64ac4294dc976a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1626264670"
+    serviceWorkerVersion: "359468782"
   }
 });
